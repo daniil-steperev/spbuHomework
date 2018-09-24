c332627f7e368d743998bf5db1966911fe97a440
Daniil Stepyrev <danik-steperev@mail.ru>
2018-09-24 19:24:50 +0300

    added task 2.3

---
+++ sem1/hw2/task3.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool gcd(int a, int b)
+{
+    while ((a > 0) && (b > 0))
+        if (a > b)
+        {
+            a = a % b;
+        }
+        else
+        {
+            b = b % a;
+        }
+    if ((a == 1) || (b == 1))
+    {
+        return true;
+    }
+    return false;
+}
+
+void countingAllFactors(int maxDenominator)
+{
+    int denominator = 1;
+    int numerator = 0;
+    while (numerator < maxDenominator)
+    {
+        while (denominator <= maxDenominator)
+        {
+            if (gcd(numerator, denominator) == true)
+            {
+                printf("%d/%d ", numerator, denominator);
+            }
+            denominator += 1;
+        }
+        numerator += 1;
+        denominator = numerator + 1;
+    }
+    printf("%d/%d", numerator, numerator);
+}
+
+int main()
+{
+    int maxDenominator = 0;
+    printf("Enter max number of the denominator: ");
+    scanf("%d", &maxDenominator);
+    countingAllFactors(maxDenominator);
+    return 0;
+}
