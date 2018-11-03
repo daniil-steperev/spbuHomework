3f832188b0235260c2745ad247ca6c9add839a37
Daniil Stepyrev <danik-steperev@mail.ru>
2018-11-03 23:43:35 +0300

    added extra library to realise coverting to postfix string, fixed borrowing names associated with 'size'

--- sem1/hw5/task3/convertToPostfix.cpp
+++ sem1/hw5/task3/convertToPostfix.cpp
@@ -2,14 +2,38 @@
 #include <limits.h>
 #include "stack.h"
 #include "convertToPostfix.h"
-
 using namespace std;
+const int length = 256;
 
-const int size = 256;
+void convertingToPostfix(char *infixString)
+{
+    Stack *stack = createStack();
+    char postfixString[length] = {'\0'};
+    for (int i = 0; i < length; i++)
+    {
+        if (infixString[i] == '\0')
+            break;
+        if (infixString[i] <= '9' && infixString[i] >= '0')
+        {
+            postfixString[freeSpace(postfixString)] = infixString[i];
+        }
+        else if (isOperation(infixString[i]))
+        {
+            push(stack, infixString[i]);
+        }
+        else if (infixString[i] == ')')
+        {
+            convertingFromBrackets(stack, postfixString);
+        }
+    }
+    convertingFromStack(stack, postfixString);
+    cout << "Postfix string: " << postfixString << endl;
+    deleteStack(stack);
+}
 
 int freeSpace(char *string)
 {
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < length; i++)
     {
         if (string[i] == '\0')
             return i;
--- sem1/hw5/task3/main.cpp
+++ sem1/hw5/task3/main.cpp
@@ -1,41 +1,13 @@
 #include <iostream>
 #include <limits.h>
-#include "stack.h"
 #include "convertToPostfix.h"
-
 using namespace std;
-const int size = 256;
 
-
-void convertingToPostfix(char *infixString)
-{
-    Stack *stack = createStack();
-    char postfixString[size] = {'\0'};
-    for (int i = 0; i < size; i++)
-    {
-        if (infixString[i] == '\0')
-            break;
-        if (infixString[i] <= '9' && infixString[i] >= '0')
-        {
-            postfixString[freeSpace(postfixString)] = infixString[i];
-        }
-        else if (isOperation(infixString[i]))
-        {
-            push(stack, infixString[i]);
-        }
-        else if (infixString[i] == ')')
-        {
-            convertingFromBrackets(stack, postfixString);
-        }
-    }
-    convertingFromStack(stack, postfixString);
-    cout << "Postfix string: " << postfixString << endl;
-    deleteStack(stack);
-}
+const int length = 256;
 
 int main()
 {
-    char infixString[size] = {'\0'};
+    char infixString[length] = {'\0'};
     cout << "Enter the expression in infix variation: ";
     cin >> infixString;
 
