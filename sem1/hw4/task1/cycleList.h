d16a857571592736843f0b955c8ca65dd627c4b9
Daniil Stepyrev <danik-steperev@mail.ru>
2018-11-08 16:22:18 +0300

    added task 4.1

---
+++ sem1/hw4/task1/cycleList.h
@@ -0,0 +1,18 @@
+#pragma once
+
+struct SortedCycleList;
+SortedCycleList *createSortedCycleList();
+
+void add(SortedCycleList *list, int value);
+void addToEmptyList(SortedCycleList *list, int value);
+void addFirst(SortedCycleList *list, int value);
+
+void removeNext(SortedCycleList *current);
+void removeElement(SortedCycleList *list, int value);
+void removeFirst(SortedCycleList *list);
+void removeByPosition(SortedCycleList *list);
+
+void changePosition(SortedCycleList *list, int shift);
+
+int getLast(SortedCycleList *list, int number);
+void deleteLoopedList(SortedCycleList *list);
