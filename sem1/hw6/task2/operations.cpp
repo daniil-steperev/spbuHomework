9b81456351f5eed71d9e273c53f1062f9c82e88a
Daniil Stepyrev <danik-steperev@mail.ru>
2018-11-24 22:29:37 +0300

    fixed troubles with file operation.cpp, operation.h

--- sem1/hw6/task2/operations.cpp
+++ sem1/hw6/task2/operations.cpp
@@ -13,11 +13,7 @@ void binaryOperations(int firstNumber, int secondNumber)
 
     printingInBinary(firstBinary, secondBinary);
 
-<<<<<<< HEAD
     sumBinary(firstBinary, secondBinary);
-=======
-    summingBinary(firstBinary, secondBinary);
->>>>>>> d72617ab2b1da9426affa18044d627aa891e07a7
     sum(firstNumber, secondNumber);
 
     delete[] firstBinary;
