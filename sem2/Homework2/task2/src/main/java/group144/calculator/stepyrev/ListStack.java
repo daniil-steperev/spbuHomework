2e04f4aee9522f8621002cbb09dc532f3a87647e
Daniil Stepyrev <danik-steperev@mail.ru>
2019-02-26 20:15:39 +0300

    added task 2 from hw 2 (calculator)

---
+++ sem2/Homework2/task2/src/main/java/group144/calculator/stepyrev/ListStack.java
@@ -0,0 +1,63 @@
+package group144.calculator.stepyrev;
+
+/**
+ * An implementation of stack that based on using linked list idea
+ * @param <Type> means type of objects that stack is consisted of
+ */
+public class ListStack<Type> implements Stack<Type> {
+    private class Node {
+        private Type value;
+        private Node next;
+
+        private Node(Type value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+
+        private Node(Type value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    private Node head = null;
+    private int length = 0;
+
+    @Override
+    public void push(Type value) {
+        if (isEmpty()) {
+            head = new Node(value);
+            length++;
+            return;
+        }
+
+        Node previousHead = head;
+        Node newHead = new Node(value, previousHead);
+
+        head = newHead;
+        length++;
+    }
+
+    @Override
+    public Type pop() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        Type returnValue = head.value;
+
+        head = head.next;
+        length--;
+        return returnValue;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    @Override
+    public int getLength() {
+        return length;
+    }
+}
---
+++ sem2/Homework2/task2/src/main/java/group144/calculator/stepyrev/Stack.java
@@ -0,0 +1,27 @@
+package group144.calculator.stepyrev;
+
+/**
+ * An interface that describes main methods of stack
+ * @param <Type> means type of value that stack is consisted of
+ */
+public interface Stack<Type> {
+    /**
+     * A method that adds new value to the top of the stack
+     * @param value means value that should be added
+     */
+    void push(Type value);
+
+    /**
+     * A method that removes top element
+     * @return means value of removed element
+     */
+    Type pop();
+
+    /**
+     * A method that checks if the stack is empty
+     * @return means true if stack is empty and false if it is not
+     */
+    boolean isEmpty();
+
+    int getLength();
+}
