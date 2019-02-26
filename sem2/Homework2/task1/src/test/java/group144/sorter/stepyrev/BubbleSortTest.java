ab2a4e07a1e3c0fc48ca7ede0e3b87319fe685a7
Daniil Stepyrev <danik-steperev@mail.ru>
2019-02-26 20:12:24 +0300

    added task 1 from hw 2 (sorter)

---
+++ sem2/Homework2/task1/src/test/java/group144/sorter/stepyrev/BubbleSortTest.java
@@ -0,0 +1,63 @@
+package group144.sorter.stepyrev;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class BubbleSortTest {
+    private static BubbleSort sorter = new BubbleSort();
+
+    @Test
+    void sortEmptyArray() {
+        int[] array = {};
+        int[] sortedArray = {};
+
+        sorter.sort(array);
+        assertArrayEquals(sortedArray, array);
+    }
+
+    @Test
+    void sortArrayConsistingFromOneNumber() {
+        int[] array = {5};
+        int[] sortedArray = {5};
+
+        sorter.sort(array);
+        assertArrayEquals(array, sortedArray);
+    }
+
+    @Test
+    void sortArrayConsistingFromTheSameNumbers() {
+        int[] array = {-5, -5, -5, -5, -5};
+        int[] sortedArray = {-5, -5, -5, -5, -5};
+
+        sorter.sort(array);
+        assertArrayEquals(array, sortedArray);
+    }
+
+    @Test
+    void sortAscendingSortedArray() {
+        int[] array = {1, 2, 3, 4, 5};
+        int[] sortedArray = {1, 2, 3, 4, 5};
+
+        sorter.sort(array);
+        assertArrayEquals(array, sortedArray);
+    }
+
+    @Test
+    void sortDescendingSortedArray() {
+        int[] array = {5, 4, 3, 2, 1};
+        int[] sortedArray = {1, 2, 3, 4, 5};
+
+        sorter.sort(array);
+        assertArrayEquals(array, sortedArray);
+    }
+
+    @Test
+    void sortRegularArray() {
+        int[] array = {4, 1, 2, 5, 1, 2, 0, -1};
+        int[] sortedArray = {-1, 0, 1, 1, 2, 2, 4, 5};
+
+        sorter.sort(array);
+        assertArrayEquals(array, sortedArray);
+    }
+}
