29874cb1cfb3ba73b9472fe28653759bd0bd98bf
Daniil Stepyrev <danik-steperev@mail.ru>
2019-02-28 19:15:43 +0300

    added task 1.2 (linked list)

---
+++ sem2/Homework1/task2/List.java
@@ -0,0 +1,184 @@
+package group144.steperev_daniil;
+
+/**
+ * A singly connected List
+*/
+public class List<Type> {
+    private class Node {
+        private Type value;
+        private Node next;
+
+        private Node(Type value) {
+            this.value = value;
+            this.next = null;
+        }
+
+        private Node(Type value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+
+        private Type getValue() {
+            return value;
+        }
+
+        private void setNextNode(Node node) {
+            this.next = node;
+        }
+
+        private Node getNextNode() {
+            return this.next;
+        }
+    }
+
+    private Node head;
+    private int length;
+
+    public List() {
+        head = null;
+        length = 0;
+    }
+
+    private boolean isEmpty() {
+        return (head == null);
+    }
+
+    /**
+     * A method adding a new element to the end of the list
+     *
+     * @param value means value of the element that user wants to add to List
+     */
+    public void add(Type value) {
+        if (isEmpty()) {
+            head = new Node(value);
+            length = 1;
+        }
+        else {
+            Node current = this.head;
+            while (current.next != null) {
+                current = current.getNextNode();
+            }
+
+            current.setNextNode(new Node(value));
+            length++;
+        }
+    }
+
+    /**
+     * A method adding a new value to the index to the list
+     *
+     * @param value means value of the element that user wants to add to List
+     * @param index means positions where new element should be inserted
+     */
+    public void add(Type value, int index) {
+        if (isEmpty()) {
+            head = new Node(value);
+            length = 1;
+            return;
+        }
+
+        if (length - 1 < index || index < 0) {
+            add(value);
+            return;
+        }
+
+        length++;
+
+        if (index == 0) {
+            Node previousHead = this.head;
+            this.head = new Node(value, previousHead);
+            return;
+        }
+
+        Node current = this.head;
+        for (int i = 0; i < index; i++) {
+            current = current.getNextNode();
+        }
+
+        current.setNextNode(new Node(value, current.next));
+    }
+
+    /**
+     *A method removing last element from the list
+     */
+    public void remove() {
+        if (isEmpty()) {
+            return;
+        }
+
+        length--;
+
+        Node current = this.head;
+        while (current.next != null) {
+            current = current.next;
+        }
+
+        current = null;
+    }
+
+    /**
+     * A method removing element from the index from the list
+     *
+     * @param index means index of the element that user wants to remove
+     */
+    public void remove(int index) {
+        if (isEmpty() || length - 1 < index) {
+            return;
+        }
+
+        if (length - 1 == index) {
+            remove();
+            return;
+        }
+
+        length--;
+
+        Node current = this.head;
+        for (int i = 0; i < index - 1; i++) {
+            current = current.getNextNode();
+        }
+
+        current.setNextNode(current.next.next);
+    }
+
+    /**
+     * A method finding the element in the list
+     *
+     * @param value means value of element that user wants to find
+     * @return index means index of element that was found in List (-1 if not found)
+     */
+    public int find(Type value) {
+        if (isEmpty()) {
+            return -1;
+        }
+
+        int index = 0;
+
+        Node current = this.head;
+        while (current != null && current.getValue() != value) {
+            current = current.getNextNode();
+            index++;
+        }
+
+        if (current == null) {
+            return -1;
+        }
+
+        return index;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    /**
+     * A method printing the list
+     */
+    public void printList() {
+        Node currentNode = head;
+        for (int i = 0; i < length; i++) {
+            System.out.print(currentNode.getValue() + " ");
+            currentNode = currentNode.getNextNode();
+        }
+    }
+}
---
+++ sem2/Homework1/task2/Main.java
@@ -0,0 +1,28 @@
+package group144.steperev_daniil;
+
+public class Main {
+    public static void main(String[] args) {
+        List list = new List();
+
+        list.add(5);
+        list.add(13);
+        list.add(123);
+        list.add(62);
+        list.add(14562);
+        list.add(984);
+
+        System.out.println("Lenth of list = " + list.getLength());
+        System.out.print("List: ");
+        list.printList();
+        System.out.println("");
+        System.out.println("Remove third element");
+        list.remove(2);
+        list.printList();
+        System.out.println("");
+        System.out.println("Find 62 in list, if it present return index (>= 0) = " + list.find(62));
+        System.out.println("Added 5 in list: ");
+        list.add(5);
+        System.out.println("List: ");
+        list.printList();
+    }
+}
