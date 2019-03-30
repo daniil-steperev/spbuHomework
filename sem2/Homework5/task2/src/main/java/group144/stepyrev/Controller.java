9be6ca13ccd389e546a7372bd66e66a68f3a7851
Daniil Stepyrev <danik-steperev@mail.ru>
2019-03-30 14:24:38 +0300

    added task 2 from hw 5 (simple calculator)

---
+++ sem2/Homework5/task2/src/main/java/group144/stepyrev/Controller.java
@@ -0,0 +1,67 @@
+package group144.stepyrev;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private TextField answer;
+
+    @FXML
+    private ChoiceBox<String> operations;
+
+    @FXML
+    private Spinner<Integer> firstNumber;
+
+    @FXML
+    private Spinner<Integer> secondNumber;
+
+    public void initialize() {
+        operations.getItems().addAll("+", "-", "*", "/");
+        operations.valueProperty().setValue("-");
+
+        operations.valueProperty().addListener(((observable, oldValue, newValue) -> calculate()));
+
+        firstNumber.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(-1000, 1000, 0, 1));
+        secondNumber.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(-1000, 1000, 0, 1));
+
+        answer.setText("0");
+
+        firstNumber.valueProperty().addListener(((observable, oldValue, newValue) -> calculate()));
+        secondNumber.valueProperty().addListener(((observable, oldValue, newValue) -> calculate()));
+    }
+
+    private void calculate() {
+        int first = firstNumber.getValue();
+        int second = secondNumber.getValue();
+        String operation = operations.getValue();
+
+        if (operation.equals("/") && second == 0) {
+            answer.setText("Error! Division by 0!");
+            return;
+        }
+
+        double result = 0;
+        switch (operation) {
+            case "+":
+                result = first + second;
+                break;
+            case "-":
+                result = first - second;
+                break;
+            case "*":
+                result = first * second;
+                break;
+            case "/":
+                result = first / second;
+                break;
+        }
+
+        answer.setText(Double.toString(result));
+    }
+
+}
---
+++ sem2/Homework5/task2/src/main/java/group144/stepyrev/Main.java
@@ -0,0 +1,29 @@
+package group144.stepyrev;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
+            primaryStage.setTitle("Simple calculator");
+            primaryStage.setScene(new Scene(root, 400, 200));
+            primaryStage.setResizable(false);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
