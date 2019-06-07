ede98523cef17c8140f4bfb304ee157a9afa51ce
Daniil Stepyrev <danik-steperev@mail.ru>
2019-06-07 09:19:00 +0300

    removed extra fields and constructor from operand node

--- sem2/Homework3/task2/src/main/java/group144/stepyrev/OperandNode.java
+++ sem2/Homework3/task2/src/main/java/group144/stepyrev/OperandNode.java
@@ -3,24 +3,10 @@ package group144.stepyrev;
 /** A class that represents an operand from the expression tree */
 public class OperandNode implements Node {
     private int value;
-    private Node leftChild;
-    private Node rightChild;
 
     /** A constructor of Operand Node */
     public OperandNode(int value) {
         this.value = value;
-    }
-
-    /**
-     * A construcot of Operand Node
-     * @param value - a value of node
-     * @param leftChild - a left child
-     * @param rightChild - a right child
-     */
-    public OperandNode(int value, Node leftChild, Node rightChild) {
-        this.value = value;
-        this.leftChild = leftChild;
-        this.rightChild = rightChild;
     }
 
     /** {@inheritDoc}*/
