368709547a3f14a2a5b3db201181c3889cbb8de1
daniil.stepyrev <danik-steperev@mail.ru>
2019-10-23 21:10:22 +0300

    cannon shoots now, trying to fix the shell's flight

--- sem3/hw3/task1/src/main/java/cannon/Shell.java
+++ sem3/hw3/task1/src/main/java/cannon/Shell.java
@@ -1,20 +1,20 @@
 package cannon;
 
-import java.util.concurrent.TimeUnit;
-
 import static java.lang.Math.pow;
 import static java.lang.StrictMath.cos;
 import static java.lang.StrictMath.sin;
 
 /** A class that represents a shell. */
 public class Shell extends GameObject {
-    private final int START_SPEED = 10;
-    private final double GRAVITY_ACCELERATION = 1.81;
+    private final double START_SPEED = 0.7;
+    private final double GRAVITY_ACCELERATION = 0.41;
 
     /** A shell's X coordinate speed. */
     private double speedX;
     /** A shell's Y coordinate speed. */
     private double speedY;
+
+    private boolean isShooting;
 
     /**
      * A constructor of the shell class.
@@ -23,21 +23,15 @@ public class Shell extends GameObject {
      */
     public Shell(double x, double y) {
         super(x, y, ShapeMatrix.SHELL);
+        isShooting = false;
     }
 
     /** A method that starts a move of the shell. */
     public void shoot(double angle) {
+        isShooting = true;
+
         speedX = START_SPEED * cos(angle);
         speedY = START_SPEED * sin(angle);
-
-        while (x <= GameApplication.WIDTH && y >= 0 && y <= GameApplication.HEIGHT && x >= 0) {
-            fly();
-            try {
-                TimeUnit.MICROSECONDS.sleep(10);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
     }
 
     public void changeCoordinates(double x, double y) {
@@ -53,7 +47,12 @@ public class Shell extends GameObject {
 
     @Override
     public void draw(GameApplication game) {
+        if (isShooting) {
+            fly();
+        }
+
         if (x < 0 || x > GameApplication.WIDTH - 1 || y < 0 || y > GameApplication.HEIGHT - 1) {
+            isShooting = false;
             return;
         }
 
