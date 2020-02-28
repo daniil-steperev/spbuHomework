a95b8cf6fafa3575e832f47ce1d74c997d9a4516
daniil.stepyrev <danik-steperev@mail.ru>
2020-02-28 09:50:25 +0300

    made fibonacci working with negative numbers

--- sem4/hw1/fibonacci.hs
+++ sem4/hw1/fibonacci.hs
@@ -2,4 +2,4 @@ fibonacci :: Int -> Int
 fibonacci 0 = 0
 fibonacci 1 = 1
 fibonacci n | n > 0 = fibonacci (n - 1) + fibonacci (n - 2)
-            | otherwise = error "number should be greater than or equal to 0 "
+            | n < 0 = -fibonacci (n + 1) + fibonacci (n + 2)
