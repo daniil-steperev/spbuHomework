7966bef3ab265a6021a98e0b1e85edbc3f9033c2
Daniil Stepyrev <43496856+daniil-steperev@users.noreply.github.com>
2020-03-08 19:23:33 +0300

    deleted extra definition

--- sem4/hw1/factorial.hs
+++ sem4/hw1/factorial.hs
@@ -1,5 +1,4 @@
 factorial :: Int -> Int
 factorial 0 = 1
-factorial 1 = 1
 factorial n | n > 0 =  n * factorial (n - 1)
             | otherwise = error "number should be greater than or equal to 0"
