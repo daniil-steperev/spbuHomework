112a00d30fdc69cc1b2e1e7ccd4a3cc074576aef
daniil.stepyrev <danik-steperev@mail.ru>
2020-05-20 14:34:38 +0300

    fixed errors, added functions of finding all suitable elements

--- sem4/test/queue.hs
+++ sem4/test/queue.hs
@@ -1,19 +1,21 @@
-data ListElement = ListElement { value :: Int,
+data QueueElement = QueueElement { value :: Int,
                                  priority :: Int
                                } deriving (Eq, Show)
                                
-getValue :: ListElement -> Int
-getValue (ListElement value priority) = value
+getValue :: QueueElement -> Int
+getValue (QueueElement value priority) = value
 
-getPriority :: ListElement -> Int
-getPriority (ListElement value priority) = priority
+getPriority :: QueueElement -> Int
+getPriority (QueueElement value priority) = priority
 
 main = do doLoop []
           
 doLoop list = do putStrLn "Enter a command 1 to add value with priority"
-                 putStrLn "Enter a command 2 to get value with inputted priority"
-                 putStrLn "Enter a command 3 to get value with max priority"
+                 putStrLn "Enter a command 2 to get first value with inputted priority"
+                 putStrLn "Enter a command 3 to get first value with max priority"
                  putStrLn "Enter a command 4 to print the queue"
+                 putStrLn "Enter a command 5 to get all values with inputted priority"
+                 putStrLn "Enter a command 6 to get all values with max priority"
                  putStrLn "Enter a command 0 to quit"
                  
                  command <- getLine
@@ -35,28 +37,52 @@ doLoop list = do putStrLn "Enter a command 1 to add value with priority"
                     '4':_ -> do putStr $ "Queue: "
                                 doPrintQueue list
                                 doLoop list
+                    '5':_ -> do putStrLn "Enter the priority of an element"
+                                priority <- getLine
+                                putStr "Values with inputted priority: "
+                                doPrintPriorityItems list (read priority) []
+                                doLoop list
+                    '6':_ -> do putStr "Values with max priority: "
+                                doPrintMaxPriorityItems list []
+                                doLoop list            
                     _     -> do putStrLn "Enter a correct command, please"
                                 doLoop list
                                 
-doAddElement :: [ListElement] -> Int -> Int -> [ListElement]
-doAddElement [] value priority = [ListElement value priority]
-doAddElement (prevElem@(ListElement curValue curPriority):xs) value priority
-    | priority >= curPriority = (ListElement value priority) : prevElem : xs
+doAddElement :: [QueueElement] -> Int -> Int -> [QueueElement]
+doAddElement [] value priority = [QueueElement value priority]
+doAddElement (prevElem@(QueueElement curValue curPriority):xs) value priority
+    | priority >= curPriority = (QueueElement value priority) : prevElem : xs
     | otherwise              = prevElem : doAddElement xs value priority
     
-doGetPriorityItem :: [ListElement] -> Int -> Maybe Int
+doGetPriorityItem :: [QueueElement] -> Int -> Maybe Int
 doGetPriorityItem [] _ = Nothing
-doGetPriorityItem ((ListElement curValue curPriority):xs) priority
+doGetPriorityItem ((QueueElement curValue curPriority):xs) priority
     | priority == curPriority = Just curValue
-    | otherwise               = doGetPriorityItem xs priority
+    | curPriority > priority  = doGetPriorityItem xs priority
+    | otherwise               = Nothing                                            -- current priority is less than the found one
     
-doGetMaxPriorityItem :: [ListElement] -> Maybe Int
-doGetMaxPriorityItem []                                      = Nothing
-doGetMaxPriorityItem ((ListElement curValue curPriority):xs) = Just curValue
+doPrintPriorityItems :: [QueueElement] -> Int -> [QueueElement] -> IO ()
+doPrintPriorityItems [] _ items = doPrintQueue $ reverse items
+doPrintPriorityItems (elem@(QueueElement curValue curPriority):xs) priority items
+    | priority == curPriority = doPrintPriorityItems xs priority (elem : items)
+    | curPriority > priority  = doPrintPriorityItems xs priority items
+    | otherwise               = doPrintQueue $ reverse items                       -- current priority is less than the found one
+    
+doPrintMaxPriorityItems :: [QueueElement] -> [QueueElement] -> IO ()
+doPrintMaxPriorityItems [] items = doPrintQueue $ reverse items
+doPrintMaxPriorityItems (x:xs) [] = doPrintMaxPriorityItems xs [x]
+doPrintMaxPriorityItems (elem@(QueueElement curValue curPriority):xs) (maxElem@(QueueElement _ maxPriority):ys)
+    | curPriority == maxPriority = doPrintMaxPriorityItems xs (elem : maxElem : ys)
+    | otherwise = doPrintQueue $ reverse $ maxElem:ys
+    
+doGetMaxPriorityItem :: [QueueElement] -> Maybe Int
+doGetMaxPriorityItem []                                       = Nothing
+doGetMaxPriorityItem ((QueueElement curValue curPriority):xs) = Just curValue
 
-doPrintQueue :: [ListElement] -> IO ()
-doPrintQueue ((ListElement curValue curPriority):[]) = do   putStr $ show $ curValue
+doPrintQueue :: [QueueElement] -> IO ()
+doPrintQueue [] = putStrLn "is empty."
+doPrintQueue ((QueueElement curValue curPriority):[]) = do  putStr $ show $ curValue
                                                             putStrLn "."  
-doPrintQueue ((ListElement curValue curPriority):xs) = do   putStr $ show $ curValue
+doPrintQueue ((QueueElement curValue curPriority):xs) = do  putStr $ show $ curValue
                                                             putStr ", "
                                                             doPrintQueue xs
